9b85effd2bc8e5eb8b2325d757934d1d8365d57a
MartinZava <zava810@gmail.com>
2022-08-31 21:29:35 -0300

    Subida entregable complementario

---
+++ main.js
@@ -0,0 +1,55 @@
+/*for (let i = 1; i <= 10; i++) {
+    let ingresarNombre = prompt ("Ingresar Nombre");
+
+    alert ("Turno Nro. " + i + " Nombre: " +  ingresarNombre);
+    
+    if (i == 10){
+        break
+    }
+}*/
+
+
+
+let nivel = prompt (`Ingrese su nivel de Javascript: 
+
+Inicial
+Intermedio
+Avanzado
+
+Ingrese ESC para salir`)
+
+while (nivel != "ESC" && nivel != "esc" && nivel != "Esc") {
+
+    switch (nivel) {
+        case `Inicial`:
+            alert ("Usted tiene clases los lunes y miercoles.");
+            break;
+
+        case `Intermedio`:
+            alert ("Usted tiene clases los martes y jueves.");
+            break;
+
+        case `Avanzado`:
+            alert ("Usted tiene clases los sabados.");
+            break;
+
+        default:
+            alert ("Ingrese un nivel.")
+            break;
+    }
+    nivel = prompt (`Ingrese su nivel de Javascript: 
+
+Inicial
+Intermedio
+Avanzado
+
+Ingrese ESC para salir`)
+
+    if (nivel === "ESC" || nivel === "esc" || nivel === "Esc") {
+        alert ("Muchas Gracias")
+    }
+}
+
+
+
+
